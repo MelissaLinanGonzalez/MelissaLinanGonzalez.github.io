034cafe4f84067eddc7e6eefd92dc806bf6889c8
Melissa Liñán <melissa16800@gmail.com>
2025-05-08 17:48:05 +0200

    actualizacion de mi cv con Fetch

---
+++ script.js
@@ -0,0 +1,12 @@
+async function cargarHtml(pagina){
+    try {
+        const response = await fetch(pagina);
+        if (!response.ok) throw new Error("No se ha podido cargar el contenido");
+        const contenido = await response.text();
+        document.getElementById("contenido-html").innerHTML = contenido;
+    } catch (error) {
+        document.getElementById("contenido-html").innerHTML = `<p>Error al cargar la página: ${error.message}</p>`;
+    }
+}
+
+window.onload = cargarHtml("index.html");
